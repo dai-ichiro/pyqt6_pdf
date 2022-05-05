bfbaca6e336e803a61b87434995bb54ba4d20a6f
hoge <hoge@deskmini>
2022-05-05 23:41:53 +0900

    modify

--- pdf.py
+++ pdf.py
@@ -1,46 +1,35 @@
 
-from PyQt6.QtCore import QUrl, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
-from PyQt6.QtWebEngineWidgets import QWebEngineView #, QWebEngineSettings
+from PyQt6.QtCore import QUrl
+from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PyQt6.QtWebEngineWidgets import QWebEngineView
 
 from os import path
  
-class MainWindow(QMainWindow):
+class Window(QMainWindow):
     def __init__(self):
-        super(QMainWindow, self).__init__()
- 
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
         self.setWindowTitle("PDF Viewer")
-        self.setGeometry(0, 28, 1000, 750)
+        self.resize(1000,750)
  
         self.webView = QWebEngineView()
         self.webView.settings().setAttribute(self.webView.settings().WebAttribute.PluginsEnabled, True)
         self.webView.settings().setAttribute(self.webView.settings().WebAttribute.PdfViewerEnabled, True)
         self.setCentralWidget(self.webView)
+        self.showDialog1()
 
-        self.webView.setUrl(QUrl("file:///D:/python/qtworks/H570.pdf#page=10"))
- 
-    def url_changed(self):
-        self.setWindowTitle(self.webView.title())
- 
-    def go_back(page:int, self) -> None:
-        
-        self.webView.page.runJavaScript(f"window.viewer.viewport_.goToPage({page})")
+    def showDialog1(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open PDF file')
+        if fname[0]:
+            print(fname[0])
+            self.webView.setUrl(QUrl("file:///" + fname[0]))
 
-    def keyPressEvent(self, e):
-        if e.key() == Qt.Key.Key_N:
-            self.webView.setUrl(QUrl("file:///D:/python/qtworks/H570.pdf#page=20"))
- 
 if __name__ == '__main__':
  
     import sys
-    app = QApplication(sys.argv)
-    win = MainWindow()
-    win.show()
-    '''
-    if len(sys.argv) > 1:
-        win.webView.setUrl(QUrl.fromLocalFile("d:\python\qtworks\H570.pdf"))
-    else:
-        win.webView.setUrl(QUrl("file:///D:/python/qtworks/H570.pdf#page=10"))
-    ''' 
- 
-    sys.exit(app.exec())
+    app = QApplication([])
+    ex = Window()
+    ex.show()
+    app.exec()
